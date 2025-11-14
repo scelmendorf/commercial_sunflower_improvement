f2553c21690ce7c39a5e9ea752a7d8afeb2a6599
Sarah Elmendorf <sarah.elmendorf@colorado.edu>
2025-11-14 15:40:48 -0700

    updated rename script so it warns about non-overwrites

--- code/12_rename_figs_ordered_for_publication.R
+++ code/12_rename_figs_ordered_for_publication.R
@@ -1,7 +1,12 @@
 # Read in .csv mapping figure names to order in manuscript
+
+# delete all files in figures/renamed_for_journal if any exist
 mapping_file <- read.csv(file.path("figure_inputs", "figure_rename_for_journal.csv"))
 for (i in 1:nrow(mapping_file)) {
   old_name <- mapping_file$filename[i]
+  if (file.exists(file.path("figures", new_name))) {
+    stop("File already exists: ", new_name)
+  }
   new_name <- file.path("renamed_for_journal", mapping_file$pub_filename[i])
   file.copy(
     file.path("figures", old_name),
