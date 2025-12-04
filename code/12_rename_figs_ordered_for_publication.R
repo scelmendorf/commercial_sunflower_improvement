de76a1dc447fb8c863cdfd31510201ff82c33c02
Sarah Elmendorf <sarah.elmendorf@colorado.edu>
2025-12-04 11:06:40 -0700

    fixed rename for journal script

--- code/12_rename_figs_ordered_for_publication.R
+++ code/12_rename_figs_ordered_for_publication.R
@@ -4,10 +4,10 @@
 mapping_file <- read.csv(file.path("figure_inputs", "figure_rename_for_journal.csv"))
 for (i in 1:nrow(mapping_file)) {
   old_name <- mapping_file$filename[i]
+  new_name <- file.path("renamed_for_journal", mapping_file$pub_filename[i])
   if (file.exists(file.path("figures", new_name))) {
     stop("File already exists: ", new_name)
   }
-  new_name <- file.path("renamed_for_journal", mapping_file$pub_filename[i])
   file.copy(
     file.path("figures", old_name),
     file.path("figures", new_name)
